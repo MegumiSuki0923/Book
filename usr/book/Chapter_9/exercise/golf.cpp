a935d349660be15887c6a7f73cbf3b0b0ae57594
MegumiSuki0923 <hy2147forever@gmail.com>
2022-03-03 23:26:22 +0800

    小小问题

---
+++ usr/book/Chapter_9/exercise/golf.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include <iostream>
+#include "golf.h"
+
+using namespace std;
+
+void setgolf(golf &g, const char *name, int hc)
+{
+	strcpy(g.fullname, name);
+	g.handicap = hc;
+}
+
+int setgolf(golf &g)
+{
+	int temp = 1;
+	cout << "Please enter the name: ";
+	cin.getline(g.fullname, 40);
+	if(strcmp(g.fullname, "") == 0)
+	{
+		return 0;
+	}
+	else
+	{
+		cout << "Please enter the handicap:";
+		cin >> g.handicap;
+		cin.get();
+		return 1;
+	}
+}
+
+void handicap(golf &g, int hc)
+{
+	g.handicap = hc;
+}
+
+void showgolf(const golf &g)
+{
+	cout << "name: " << g.fullname << endl << "handicap: " << g.handicap << endl;
+	cout << "---------Already done---------" << endl;
+}
