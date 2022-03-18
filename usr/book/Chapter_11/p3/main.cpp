7faf11993e99f9044755668fdc7482d1efb02139
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-03-18 17:28:22 +0800

    update

--- usr/book/Chapter_11/p3/main.cpp
+++ usr/book/Chapter_11/p3/main.cpp
@@ -2,7 +2,6 @@
 #include "vector.h"
 #include <cstdlib>
 #include <ctime>
-#include <fstream>
 
 using namespace VECTOR;
 
@@ -19,47 +18,38 @@ int main()
 	unsigned int min = 0;
 	unsigned int sum = 0;
 
-	ofstream fout;
-	fout.open("randwalk.txt");
-
-	//改变随机数的种子 没有这行的话，你编译一次以后，值一直不会变
 	srand(time(NULL));
 
-	cout << "Enter target distance ";
-	cin >> darget;
-	cout << "Enter the length: ";
+	cout << "How many times do you want to try: " << endl;
+	cin >> N;
+	cout << "Enter the step length: ";
 	cin >> dstep;
 
-	fout << "Target distance: " << target << ", step size: " << dstep << endl;
-
+	for (int i = 0; i < N; i++)
+	{
 		while (result.magval() < target)
 		{
-						       //这里发生了重载 左侧是oftream 右侧是类
-			fout << steps << ": (x, y) = " << result << endl;
 			direction = rand() % 360;
 			step.reset(dstep, direction, Vector::POL);
 			result = result + step;
 			steps++;
 		}
 
-		cout << "After " << steps << " steps, the sujects has following location.\n";
-		cout << result;
-
-		fout << "After " << steps << " steps, the sujects has following location.\n";
-		fout << result;
-
-		result.polar_mode();
-		cout << result << endl;
-
-		fout << result;
-		fout << "Average outward distance per step = " << result.magval() /steps << endl;
+		max = (max > steps) ? max : steps;
+		if (min == 0)
+			min = max;
+		else
+			min = (min < steps) ? min : steps;
+		sum += steps;
 
 		steps = 0;
 		result.reset(0.0, 0.0);
-		cout << "Enter target distance(q to quit): ";
 	}
 
+	cout << "The max is " << max << endl;
+	cout << "The min is " << min << endl;
+	cout << "The avarage is " << (double)sum / N << endl;
+ 
 	cout << "Bye!" << endl;
-
 	return 0;
 }
