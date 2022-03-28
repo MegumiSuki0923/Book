2034eaad507fc0d21a34d06ca4e0efecdd7558c4
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-03-28 17:31:01 +0800

    update

--- usr/book/Chapter_13/dma/usedma.cpp
+++ usr/book/Chapter_13/dma/usedma.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "dma.h"
+#include <iostream>
 
 using namespace std;
 
@@ -10,27 +10,26 @@ int main(void)
 	cout << shirt;
 	cout << "--------------------------------------------" << endl;
 
-	lacksDMA ballon("Blimpo",4 , "red");
+	lacksDMA ballon("Blimpo", 4, "red");
 	cout << "Displaying lacksDMA object(ballon): " << endl;
 	cout << ballon;
 	cout << "--------------------------------------------" << endl;
 
 	lacksDMA ballon2(ballon);
 	cout << "Displaying lacksDMA object(ballon2): " << endl;
-        cout << ballon2;
-        cout << "--------------------------------------------" << endl;
+	cout << ballon2;
+	cout << "--------------------------------------------" << endl;
 
 	hasDMA map("Keys", 5, "Mercator");
 	cout << "Displaying hasDMA object: " << endl;
 	cout << map;
 	cout << "--------------------------------------------" << endl;
 
-	//
+	//这里使用不是赋值运算符，因为这里在初始化，使用的应该是复制运算符
 	hasDMA map2 = map;
-	cout << "Displaying hasDMA object: " << endl;
-        cout << map;
-        cout << "--------------------------------------------" << endl;
+	cout << "Displaying hasDMA object(map2): " << endl;
+	cout << map2;
+	cout << "--------------------------------------------" << endl;
 
-  	return 0;
+	return 0;
 }
-
