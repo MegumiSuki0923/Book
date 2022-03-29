f3314a9f774a2e9fe1bfde89d6cd1c0e09eb2c5a
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-03-29 16:08:16 +0800

    none

--- usr/book/Chapter_13/p1/classic.h
+++ usr/book/Chapter_13/p1/classic.h
@@ -7,32 +7,33 @@ using namespace std;
 
 class Cd
 {
-	private:
-		char performers[50];
-		char label[20];
-		int selections;
-		double playtime;
-	public:
-		Cd(char *s1, char *s2, int n, double x);
-		Cd(const Cd &d);
-		Cd();
-		virtual ~Cd() {};
-		virtual void Report() const;
-		Cd &operator=(const Cd &d);
+private:
+	char performers[50];
+	char label[20];
+	int selections;
+	double playtime;
+
+public:
+	Cd(char *s1, char *s2, int n, double x);
+	Cd(const Cd &d);
+	Cd();
+	virtual ~Cd(){};
+	virtual void Report() const;
+	Cd &operator=(const Cd &d);
 };
 
 class Classic : public Cd
 {
-	private:
-		char name[40];
-	public:
-		Classic(char *s1, char *s2, char *s3, int n, double x);
-		Classic(const Classic &c);
-		Classic();
-		Classic(Classic &c);
-		~Classic() {};
-		virtual void Report() const;
-		Classic &operator=(const Classic &c);
+private:
+	char name[40];
+
+public:
+	Classic(char *s1, char *s2, char *s3, int n, double x);
+	Classic(const Classic &c);
+	Classic();
+	~Classic(){};
+	virtual void Report() const;
+	Classic &operator=(const Classic &c);
 };
 
 #endif
