f19a9e4cae698158f5798cb3459d04a85a403a32
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-03-31 19:17:09 +0800

    haha

--- usr/book/Chapter_14/tempparm/tempparm.cpp
+++ usr/book/Chapter_14/tempparm/tempparm.cpp
@@ -3,15 +3,16 @@
 
 using namespace std;
 
-template<template<class T>class Thing>
+template <template <class T> class Thing>
 class Crab
 {
-	private:
-		Thing<int> s1;
-		Thing<double> s2;
-	public:
-		bool push(int a, double x) {return (s1.push(a) && s2.push(x));}
-		bool pop(int &a, double &x) {return (s1.pop(a) && s2.pop(x));}
+private:
+	Thing<int> s1;
+	Thing<double> s2;
+
+public:
+	bool push(int a, double x) { return (s1.push(a) && s2.push(x)); }
+	bool pop(int &a, double &x) { return (s1.pop(a) && s2.pop(x)); }
 };
 
 int main(void)
@@ -22,13 +23,13 @@ int main(void)
 	Crab<Stack> nebula;
 
 	cout << "Enter int & double pairs, such as 2 3.14(0 0 to end): " << endl;
-	while((cin >> ni >> nb) && (ni > 0 && nb > 0))
+	while ((cin >> ni >> nb) && (ni > 0 && nb > 0))
 	{
-		if(!nebula.push(ni, nb));
+		if (!nebula.push(ni, nb))
 			break;
 	}
 
-	while(nebula.pop(ni, nb))
+	while (nebula.pop(ni, nb))
 		cout << ni << ", " << nb << endl;
 
 	return 0;
