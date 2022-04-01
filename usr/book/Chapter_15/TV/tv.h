ccfd8fd9478c327d3699653db0cce2e45479940d
‘lain’ <hy2147forever@gmail.com>
2022-04-01 20:43:27 +0800

    haha

---
+++ usr/book/Chapter_15/TV/tv.h
@@ -0,0 +1,49 @@
+#ifndef __TV_H__
+#define __TV_H__
+
+#include <iostream>
+
+class Tv
+{
+	private:
+		enum{off, on};
+		enum{MinVal, MaxVal = 20};
+		enum{MinChan = 1, MaxChan = 100};
+		enum{TV, DVD};
+
+		int stage;
+		int volume;
+		int channel;
+		int input;
+
+	public:
+		Tv(int s = off) : stage(s), volume(5), channel(2), input(TV) {}
+		void onoff() {stage = (stage == off) ? on : off;}
+		bool volup();
+		bool voldown();
+		void chanup();
+		void chandown();
+		void setinput() {input = (input == TV) ? DVD : TV;}
+		void show_settings() const;
+
+		//友元类
+		friend class Remote;
+};
+
+class Remote
+{
+	private:
+		int mode;
+
+	public:
+		Remote(int m = Tv::TV) : mode(m) {}
+		void onoff(Tv &t) {t.onoff();}
+		bool volup(Tv &t) {return t.volup();}
+		bool voldown(Tv &t) {return t.voldown();}
+		void chanup(Tv &t) {t.chanup();}
+		void chandown(Tv &t) {t.chandown();}
+		void set_channel(Tv &t, int c) {t.channel = c;}
+		void set_input(Tv &t) {t.setinput();}
+};
+
+#endif
