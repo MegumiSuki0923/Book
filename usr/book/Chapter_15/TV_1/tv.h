c467f6238314fe10e448134ee69872f86ecb0960
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-04-01 20:54:09 +0800

    haha

--- usr/book/Chapter_15/TV_1/tv.h
+++ usr/book/Chapter_15/TV_1/tv.h
@@ -3,6 +3,29 @@
 
 #include <iostream>
 
+class Tv;
+
+class Remote
+{
+private:
+	int mode;
+
+	enum{off, on};
+	enum{MinVal, MaxVal = 20};
+	enum{MinChan = 1, MaxChan = 100};
+	enum{TV, DVD};
+
+public:
+	Remote(int m = TV) : mode(m) {}
+	void onoff(Tv &t);
+	bool volup(Tv &t);
+	bool voldown(Tv &t);
+	void chanup(Tv &t);
+	void chandown(Tv &t);
+	void set_channel(Tv &t, int c);
+	void set_input(Tv &t);
+};
+
 class Tv
 {
 	private:
@@ -30,20 +53,12 @@ class Tv
 		friend void Remote::set_channel(Tv &t, int c);
 };
 
-class Remote
-{
-	private:
-		int mode;
-
-	public:
-		Remote(int m = Tv::TV) : mode(m) {}
-		void onoff(Tv &t) {t.onoff();}
-		bool volup(Tv &t) {return t.volup();}
-		bool voldown(Tv &t) {return t.voldown();}
-		void chanup(Tv &t) {t.chanup();}
-		void chandown(Tv &t) {t.chandown();}
-		void set_channel(Tv &t, int c) {t.channel = c;}
-		void set_input(Tv &t) {t.setinput();}
-};
+inline void Remote::onoff(Tv &t) { t.onoff(); }
+inline bool Remote::volup(Tv &t) { return t.volup(); }
+inline bool Remote::voldown(Tv &t) { return t.voldown(); }
+inline void Remote::chanup(Tv &t) { t.chanup(); }
+inline void Remote::chandown(Tv &t) { t.chandown(); }
+inline void Remote::set_channel(Tv &t, int c) { t.channel = c; }
+inline void Remote::set_input(Tv &t) { t.setinput(); }
 
 #endif
