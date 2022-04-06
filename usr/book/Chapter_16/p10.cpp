e8e389436a79a7ed93f1859fba5dab347f660bd5
lain <hy2147forever@gmail.com>
2022-04-06 09:43:10 +0800

    hah

---
+++ usr/book/Chapter_16/p10.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Review
+{
+	string title;
+	int rating;
+	double price;
+};
+
+bool FillReview(Review &r);
+void ShowReview(const Review &r);
+
+int main(void)
+{
+	vector<shared_ptr<Review>> books;	//vector<Review> books;
+	Review temp;
+
+	while(FillReview(temp))
+	{
+		shared_ptr<Review> pd = new Review(temp);
+		books.push_back(pd);
+	}
+
+	int num = books.size();
+	if(num > 0)
+	{
+		
+	
+	/*
+		cout << "You entered the following:" << endl;
+		for(int i = 0; i < num; i++)
+			ShowReview(books[i]);
+
+		cout << "Use iterator:" << endl;
+		vector<Review>::iterator pr;
+		for(pr = books.begin(); pr != books.end(); pr++)
+			ShowReview(*pr);
+
+		vector<Review> oldlist(books);
+		if(num > 3)
+		{
+			cout << "After erasing:" << endl;
+			oldlist.erase(oldlist.begin() + 1, oldlist.begin() + 3);
+			for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
+				ShowReview(*pr);
+
+			cout << "After insert:" << endl;
+                	oldlist.insert(oldlist.begin(), books.begin() + 1, books.begin() + 2);
+                	for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
+                        	ShowReview(*pr);
+		}
+
+		books.swap(oldlist);
+		cout << "After swap:" << endl;
+		for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
+		                       ShowReview(*pr);
+	*/
+	}
+
+
+	return 0;
+}
+
+bool FillReview(Review &r)
+{
+	cout << "Enter book title (enter quit to quit): ";
+	getline(cin, r.title);
+	if(r.title == "quit")
+		return false;
+	cout << "Enter book rating: ";
+	cin >> r.rating;
+	cout << "Enter book's price: ";
+	cin>>r.price;
+	
+	if(!cin)
+		return false;
+	while(cin.get() != '\n');
+	return true;
+}
+
+void ShowReview(const Review &r)
+{
+	cout << "title: " << r.title << "\trating: " << r.rating << endl;
+}
