cb1dff0d43ba209262862bf6ceaa56498679ed2c
‘lain’ <hy2147forever@gmail.com>
2022-04-06 10:33:46 +0800

    no

--- usr/book/Chapter_16/p10.cpp
+++ usr/book/Chapter_16/p10.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <memory>
 
 using namespace std;
 
@@ -13,6 +15,8 @@ struct Review
 
 bool FillReview(Review &r);
 void ShowReview(const Review &r);
+bool worseThan(const shared_ptr<Review> &pt1, const shared_ptr<Review> &pt2);
+bool cheaperThan(const shared_ptr<Review> &pt1, const shared_ptr<Review> &pt2);
 
 int main(void)
 {
@@ -21,46 +25,54 @@ int main(void)
 
 	while(FillReview(temp))
 	{
-		shared_ptr<Review> pd = new Review(temp);
+		shared_ptr<Review> pd(new Review(temp));
 		books.push_back(pd);
 	}
 
 	int num = books.size();
 	if(num > 0)
 	{
-		
-	
-	/*
-		cout << "You entered the following:" << endl;
-		for(int i = 0; i < num; i++)
-			ShowReview(books[i]);
+		cout << "Choose the way you want to sort the boos: " << endl;
+		cout << "1:original order	2:alphabet sort" << endl;
+		cout << "3:rating up order	4:rating down order" << endl;
+		cout << "5:price up order	6:price down order" << endl;
+		cout << "7:quit" << endl;
 
-		cout << "Use iterator:" << endl;
-		vector<Review>::iterator pr;
-		for(pr = books.begin(); pr != books.end(); pr++)
-			ShowReview(*pr);
-
-		vector<Review> oldlist(books);
-		if(num > 3)
+		int choice;
+		while(cin >> choice)
 		{
-			cout << "After erasing:" << endl;
-			oldlist.erase(oldlist.begin() + 1, oldlist.begin() + 3);
-			for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
-				ShowReview(*pr);
-
-			cout << "After insert:" << endl;
-                	oldlist.insert(oldlist.begin(), books.begin() + 1, books.begin() + 2);
-                	for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
-                        	ShowReview(*pr);
+			switch(choice)
+			{
+				case 1:
+					for_each(books.begin(), books.end(), ShowReview);
+					break;
+				case 2:
+					sort(books.begin(), books.end());
+					for_each(books.begin(), books.end(), ShowReview);
+					break;
+				case 3:
+					sort(books.begin(), books.end(), worseThan);
+					for_each(books.begin(), books.end(), ShowReview);
+					break;
+				case 4:
+					sort(books.begin(), books.end(), worseThan);
+					reverse(books.begin(), books.end());
+                                       	for_each(books.begin(), books.end(), ShowReview);
+                                       	break;
+				case 5:
+					sort(books.begin(), books.end(), cheaperThan);
+                                       	for_each(books.begin(), books.end(), ShowReview);
+                                       	break;
+				case 6:
+					sort(books.begin(), books.end(), cheaperThan);
+                                       	reverse(books.begin(), books.end());
+                                        for_each(books.begin(), books.end(), ShowReview);
+                                        break;
+				default:
+					cout << "Done!" << endl;
+			}
 		}
-
-		books.swap(oldlist);
-		cout << "After swap:" << endl;
-		for(pr = oldlist.begin(); pr != oldlist.end(); pr++)
-		                       ShowReview(*pr);
-	*/
 	}
-
 
 	return 0;
 }
@@ -75,14 +87,40 @@ bool FillReview(Review &r)
 	cin >> r.rating;
 	cout << "Enter book's price: ";
 	cin>>r.price;
-	
+
 	if(!cin)
 		return false;
 	while(cin.get() != '\n');
 	return true;
 }
 
-void ShowReview(const Review &r)
+void ShowReview(const shared_ptr<Review> &pt)
 {
-	cout << "title: " << r.title << "\trating: " << r.rating << endl;
+	cout << "title: " << pt->title << "\trating: " << pt->rating << "\tprice: " << pt->price << endl;
+}
+
+bool operator<(const shared_ptr<Review> &pt1, const shared_ptr<Review> &pt2)
+{
+	if(pt1->title < pt2->title)
+		return true;
+	else if(pt1->title == pt2->title && pt1->rating < pt2->rating)
+		return true;
+	else
+		return false;
+}
+
+bool worseThan(const shared_ptr<Review> &pt1, const shared_ptr<Review> &pt2)
+{
+	if(pt1->rating < pt2->rating)
+		return true;
+	else
+		return false;
+}
+
+bool cheaperThan(const shared_ptr<Review> &pt1, const shared_ptr<Review> &pt2)
+{
+	 if(pt1->price < pt2->price)
+               	return true;
+       	else
+               	return false;
 }
