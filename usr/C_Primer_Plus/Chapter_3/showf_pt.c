66360cfdcb9301121d61df279923e4f17eb8126c
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-04-11 11:31:45 +0800

    hi

---
+++ usr/C_Primer_Plus/Chapter_3/showf_pt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+        float abort = 32000.0;
+        double abet = 2.14e9;
+
+        printf("%f can be writen %e.\n", abort, abort);
+        printf("%f can be writen %e.\n", abet, abet);
+
+        return 0;
+}
