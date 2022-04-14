3e0c7021c48dc0f45cfe351b61d8fc22c136d8bd
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-04-14 17:20:50 +0800

    hithere

---
+++ usr/C_Primer_Plus/Chapter_5/devision.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("integer devision 5 / 4 is %d\n", 5/4);
+        printf("integer devision 8 / 4 is %d\n", 8 / 4);
+        printf("float devision 7.0 / 4.0 is %.2f\n", 7.0/4.0);
+
+        return 0;
+}
