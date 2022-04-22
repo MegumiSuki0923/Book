d929def6d96b7a306cf54fec659622bd607e484e
“Laingit config --global user.email“hy2147forever@gmail.com <hy2147forever@gmail.com>
2022-04-22 15:51:24 +0800

    1

--- usr/C_Primer_Plus/Chapter_7/p9.c
+++ usr/C_Primer_Plus/Chapter_7/p9.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+int isprime(int n);
+
 int main(void)
 {
         int num;
 
         printf("Please enter a number:\n");
         scanf("%d", &num);
-        for(int i = 2; i <= num; i++)
+        for (int i = 2; i <= num; i++)
         {
-                if(isprime(i))
-                        printf("%d is prime\t", i);
+                if (isprime(i))
+                        printf("%d ", i);
         }
         printf("\n");
 
@@ -18,5 +20,10 @@ int main(void)
 
 int isprime(int n)
 {
-        if(n % 2)
+        for (int i = 2; i < n; i++)
+        {
+                if (n % i == 0)
+                        return 0;
+        }
+        return 1;
 }
