58b174248bdc80ffeedeea0d180c5d324ae69ccf
Your Lain <hy2147forver@gmail.com>
2022-05-22 16:40:00 +0800

    1

--- usr/C_Primer_Plus/Chapter_11/p14.c
+++ usr/C_Primer_Plus/Chapter_11/p14.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define SIZE	81
+
+int my_atoi(char *ptr);
 
 int main(int argc, char *argv[])
 {
 	char str[SIZE];
+	puts("Please enter a number string:");
 	while(gets(str) && str[0] != '\0')
 	{
-		my_atoi(str);
+		printf("%d\n", my_atoi(str));
+		puts("Next again");
 	}
 
 	return 0;
+}
+
+int my_atoi(char *ptr)
+{
+	int sign = 1, num = 0;
+
+	while(isspace(*ptr))
+		ptr++;
+	if(*ptr == '-')
+	{
+		sign = -1;
+		ptr++;
+	}
+	if(*ptr == '+')
+	{
+		sign = 1;
+		ptr++;
+	}
+	else
+	{
+		while(isdigit(*ptr))
+		{
+			ptr++;
+			num = (num * 10) + (*ptr - '0');
+		}
+	}
+
+	return num*sign;
 }
