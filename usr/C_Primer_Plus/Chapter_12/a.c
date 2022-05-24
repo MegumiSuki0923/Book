b37690e60b8f5a150aa1e17ae78620988b20838c
Lain <hy2147forever@gmail.com>
2022-05-24 16:36:11 +0800

    1

---
+++ usr/C_Primer_Plus/Chapter_12/a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "b.h"
+
+int time = 10;
+
+int main(void)
+{
+        loop();
+
+        return 0;
+}
---
+++ usr/C_Primer_Plus/Chapter_12/b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+extern int times;
+
+void loop(void)
+{
+        int i = 0;
+        for (int i = 0; i < times; i ++)
+                puts("Hello world!");
+        
+}
