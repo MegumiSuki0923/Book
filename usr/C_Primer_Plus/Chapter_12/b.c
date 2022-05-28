601296c07274bec46c58639ec4f253f7d04dc59d
Lain <hy2147forever@gmail.com>
2022-05-28 13:24:24 +0800

    1

--- usr/C_Primer_Plus/Chapter_12/b.c
+++ usr/C_Primer_Plus/Chapter_12/b.c
@@ -5,7 +5,7 @@ extern int times;
 void loop(void)
 {
         int i = 0;
-        for (int i = 0; i < times; i ++)
+        for (int i = 0; i < times; i++)
                 puts("Hello world!");
-        
 }
+ n 
