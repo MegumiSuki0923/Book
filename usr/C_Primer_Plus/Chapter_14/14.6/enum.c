3c4a52b079bd1e3f1d9bf671ba08a1d75a85d810
Lain <hy2147forever@gmail.com>
2022-06-16 10:07:20 +0800

    1

--- usr/C_Primer_Plus/Chapter_14/14.6/enum.c
+++ usr/C_Primer_Plus/Chapter_14/14.6/enum.c
@@ -1,20 +1,64 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define LEN	30
 
 enum spectrum {
 	red, orange, yellow, green, blue, violet
 };
+const char *colors = {
+    "red", "orange", "yellow", "green", "blue", "violet"
+};
 
 int main(void)
 {
 	char choice[LEN];
 	int color;
+	bool color_is_found = false;
 
 	puts("Enter a color (empty line to quit):");
 	while(gets(choice) && choice[0] != '\0')
 	{
-		for(co)
+		for(color = 0; color <= violet; color++)
+		{
+			if((strcmp(choice, colors[color])) == 0)
+			{
+				color_is_found = true;
+				break;
+			}
+		}
+		if(color_is_found)
+		{
+			switch (color)
+			{
+			case red:
+				puts("Roses are red");
+				break;
+			case orange:
+				puts("Poppies are orange");
+				break;
+			case yellow:
+				puts("Sunflowers are yellow");
+				break;
+			case green:
+				puts("Grass is green");
+				break;
+			case blue:
+				puts("Bluebell are blue");
+				break;
+			case violet:
+				puts("Violets are violet");
+				break;
+			default:
+				break;
+			}
+		}
+		else
+			printf("I don't know the color %d.\n", color);
+		
+		color_is_found = false;
+		printf("Next color (Enter empty to quit):");
 	}
 
 	return 0;
