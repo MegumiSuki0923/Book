acde83c9dd0bdbe043b5bdf894cc2097398f252e
Lain <hy2147forever@gmail.com>
2022-06-22 15:37:22 +0800

    1

--- usr/code/stack/myself.c
+++ usr/code/stack/myself.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int reverse_polish[512];
+int stack[512];
 int top = 0;
 
 void push(int n);
@@ -10,53 +10,53 @@ int is_empty(void);
 
 int main(void)
 {
-	char a[100];
-	int len;
-	int i;
-	int n1, n2;
+        char a[100];
+        int len;
+        int i;
+        int n1, n2;
 
-	puts("please enter a reverse polish expression:");
-	gets(a);
-	len = strlen(a);
+        puts("Plese enter the reverse polish expression:");
+        gets(a);
+        len = strlen(a);
 
-	for(i = 0; i < len; i++)
-	{
-		if(a[i] >= '0' && a[i] <= '9')
-			push(a[i] - '0');
-		else
-		{
-			n2 = pop();
-			n1 = pop();
-			switch(reverse_polish[i])
-			{
-				case '+':
-					push(n1 + n2);
-					break;
-				case '-':
-					push(n1 - n2);
-					break;
-				case '*':
-					push(n1 * n2);
-					break;
-			}
-		}
-	}
-	printf("result = %d\n", pop());
+        for(i = 0; i < len; i++)
+        {
+                if(a[i] >= '0' && a[i] <= '9')
+                        push(a[i] - '0');
+                else
+                {
+                        n2 = pop();
+                        n1 = pop();
+                        switch(a[i])
+                        {
+                                case '+':
+                                        push(n1 + n2);
+                                        break;
+                                case '-':
+                                        push(n1 - n2);
+                                        break;
+                                case '*':
+                                        push(n1 * n2);
+                                        break;
+                        }
+                }
+        }
+        printf("result is %d\n", pop());
 
-	return 0;
+        return 0;
 }
 
 void push(int n)
 {
-	reverse_polish[top++] = n;
+        stack[top++] = n;
 }
 
 int pop(void)
 {
-	return reverse_polish[--top];
+        return stack[--top];
 }
 
 int is_empty(void)
 {
-	return top == 0;
+        return top == 0;
 }
