e2a937d00c3582a9da426549791945b87accf019
‘lain’ <hy2147forever@gmail.com>
2022-07-22 22:58:57 +0800

    1

--- usr/data_struct/MyTree/huffman_tree.c
+++ usr/data_struct/MyTree/huffman_tree.c
@@ -7,29 +7,41 @@ struct HuffmanTree
 };
 
 void print_huffman_tree(const struct HuffmanTree H[], int n);
-int find_first(const struct HuffmanTree H[], int n);
-int find_second(const struct HuffmanTree H[], int n);
+void select(const struct HuffmanTree H[], int n, int *first, int *second);
 
 int main(void)
 {
 	int n;
 	int i, j;
+	int first, second;
 
-	printf("How many nodes do the Huffman Tree have?\n");
+	printf("How many leaves nodes do the Huffman Tree have?\n");
 	scanf("%d", &n);
 
-	struct HuffmanTree H[2 * n];
-	for(i = 1; i < n * 2; i++)
+	// 初始化哈夫曼树
+	struct HuffmanTree H[2*n-1];
+	for(i = 0; i < n*2-1; i++)
 		H[i].parent = H[i].lchild = H[i].rchild = 0;
-	for(i = 1; i < n + 1; i++)
+	for(i = 0; i < n; i++)
 	{
-		printf("%d weight:  ", i);
+		printf("%d weight:  ", i+1);
 		scanf("%d", &H[i].weight);
 	}
 
-	i = find_first(H, n);
-	printf("i = %d\n", i);
-	i = find_second(H, n-1);
+	for(i = n; i < 2*n-1; i++)
+	{
+		select(H, i, &first, &second);
+
+		// 在找到最小和第二小的下标后，开始操作
+		H[i].weight = H[first].weight + H[second].weight;
+		H[i].lchild = first;
+		H[i].rchild = second;
+
+		H[first].parent = i;
+		H[second].parent = i;
+	}
+
+	print_huffman_tree(H, n);
 
 	return 0;
 }
@@ -39,26 +51,33 @@ void print_huffman_tree(const struct HuffmanTree H[], int n)
 {
 	int i;
 
-	for(i = 1; i < n; i++)
-		printf("%d: %d %d %d %d\n", i, H[i].weight, H[i].parent, H[i].lchild, H[i].rchild);
+	for(i = 0; i < 2*n-1; i++)
+		printf("%2d: %3d %3d %3d %3d\n", i, H[i].weight, H[i].parent, H[i].lchild, H[i].rchild);
 }
 
-int find_first(const struct HuffmanTree H[], int n)
+// 这里的n是当前parent==0的叶子数
+void select(const struct HuffmanTree H[], int n, int *first, int *second)
 {
 	int i;
+	int min;
 
-	int min = H[1].weight;
-	int ans = 1;
-	for(i = 1; i < 2*n; i++)
+	min = H[0].weight;
+	for(i = 1; i < n; i++)
 	{
-		if(H[i].weight < min && H[i].weight > 0)
+		if(H[i].parent == 0 && H[i].weight < min)
 		{
-			ans = i;
 			min = H[i].weight;
+			*first = i;
 		}
 	}
-	return ans;
-}
 
-int find_second(const struct HuffmanTree H[], int n)
-{}
+	min = H[0].weight;
+	for(i = 1; i < n; i++)
+	{
+		if(H[i].parent == 0 && H[i].weight < min && i != *first)
+		{
+			min = H[i].weight;
+			*second = i;
+		}
+	}
+}
